03ca7ee5f2b7d30ecc463807689d1cd871d33bf9
Thomas Lienbacher <lienbacher.tom@gmail.com>
2024-01-14 19:44:28 +0100

    Added hydrocarbon graph and dot file creation

--- README.md
+++ README.md
@@ -9,10 +9,70 @@ This project was created with [Rust](https://www.rust-lang.org/) version 1.75.0.
 
 1. Install Rust using [rustup](https://www.rust-lang.org/tools/install) (recommended)
 2. Clone this repository `git clone https://github.com/thomaslienbacher/color-reduction.git`
-3. Run `cargo run --release` in the repository folder, this will build and run the project
+3. Use `cargo build --release` in the repository folder, this will build and place the binary
+   into `<repo>/target/release`
+4. Alternatively use `cargo run --release` to build and run the project
 
-The binary is placed in the `<repo>/target/release` folder and can alternatively 
-be just build using `cargo build --release`.
+Use `color-reduction --help` to get information on usage.
+
+## Running
+
+Running the program with no args will run the test case.
+This will create a complete graph
+with 200 nodes and then verify that each node has separate color
+using assertions.
+
+```shell
+color-reduction 
+```
+
+Alternatively one can specify which graph should be generated
+using the `-m` option, the values are
+`complete-graph`, `chain` or `hydrocarbon`.
+A chain graph is simply a graph where each node is
+connected to next similar to a linked list (max degree is 2).
+A hydrocarbon graph is similar to how hydrocarbon molecules
+are made up, it's basically a chain of carbon atoms
+with hydrogen attached to them.
+The number of nodes in the graph can also be specified.
+To get additional information about the algorithm execution
+use the verbose flag `-v`.
+
+## Examples
+
+### Run on a complete graph with 100 nodes
+
+```shell
+color-reduction -m complete-graph -n 500
+```
+
+This takes around 100ms on my system.
+
+#### Run on a chain graph with 3000 nodes
+
+```shell
+color-reduction -m chain -n 3000
+```
+
+This takes around 10ms on my system.
+
+#### Run on a hydrocarbon graph with 4000 nodes
+
+```shell
+color-reduction -m chain -n 4000
+```
+
+This takes around 10ms on my system.
+
+## Visualizing graph
+
+Using the `-d` option one can specify a file
+that will be used to generate a graphviz dot file.
+This file can then be viewed using `xdot`
+or converted to a pdf file using
+`dot -Tpdf -o graph.pdf graph.dot`.
+The graph nodes are colored using
+a randomly generated palette of colors.
 
 ## Previous version
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashSet;
+use std::fs::OpenOptions;
+use std::io::Write;
 
+use clap::{Parser, ValueEnum};
+use rand::{Rng, thread_rng};
+use rand::distributions::Uniform;
 use rand::prelude::IteratorRandom;
-use rand::thread_rng;
 use rs_graph::{Builder, VecGraph};
 use rs_graph::traits::{FiniteGraph, Indexable};
 use rs_graph::vecgraph::VecGraphBuilder;
-use clap::{Parser, ValueEnum};
 
 use crate::Coloring::{Candidate, Permanent};
 
@@ -63,7 +66,7 @@ fn complete_graph(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
 }
 
 /// creates a graph that is a chain of vertices with `num_nodes` vertices
-/// the degree has max degree 2
+/// the graph has max degree 2
 /// returns the graph, a vector of nodes and delta (max degree)
 fn chain(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
     let mut nodes = Vec::with_capacity(num_nodes);
@@ -80,16 +83,82 @@ fn chain(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
         g.add_edge(g_nodes[i + 1], g_nodes[i]);
     }
 
-    (g.into_graph(), nodes, 2)
+    (g.into_graph(), nodes, (num_nodes - 1).min(2))
 }
 
-fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<Node>, delta: usize, verbose: bool) {
+/// creates a graph that is similar to hydrocarbon chains
+/// it will try to make the chain as long as possible using `num_nodes` nodes
+/// it there aren't enough nodes some carbon atoms will not have all hydrogen neighbors
+/// the graph has max degree 4
+/// returns the graph, a vector of nodes and delta (max degree)
+fn hydrocarbon(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
+    let mut nodes = Vec::with_capacity(num_nodes);
+    let mut g = VecGraphBuilder::new();
+
+    let g_nodes = g.add_nodes(num_nodes);
+    for n in &g_nodes {
+        nodes.push(N(n.index()));
+    }
+
+    let mut node_counter = 0;
+
+    // add first hydrogen [0] and carbon [1] bond
+    if num_nodes >= 2 {
+        g.add_edge(g_nodes[0], g_nodes[1]);
+        g.add_edge(g_nodes[1], g_nodes[0]);
+    }
+    node_counter += 2;
+    let mut last_carbon = 1;
+
+    loop {
+        if node_counter >= num_nodes {
+            break;
+        }
+
+        let top_hydrogen = last_carbon + 1;
+        let bottom_hydrogen = last_carbon + 2;
+        let next_carbon = last_carbon + 3;
+
+        // add top hydrogen to last carbon
+        g.add_edge(g_nodes[last_carbon], g_nodes[top_hydrogen]);
+        g.add_edge(g_nodes[top_hydrogen], g_nodes[last_carbon]);
+
+        node_counter += 1;
+        if node_counter == num_nodes {
+            break;
+        }
+
+        // add bottom hydrogen to last carbon
+        g.add_edge(g_nodes[last_carbon], g_nodes[bottom_hydrogen]);
+        g.add_edge(g_nodes[bottom_hydrogen], g_nodes[last_carbon]);
+
+        node_counter += 1;
+        if node_counter == num_nodes {
+            break;
+        }
+
+        // add new carbon or last hydrogen
+        g.add_edge(g_nodes[last_carbon], g_nodes[next_carbon]);
+        g.add_edge(g_nodes[next_carbon], g_nodes[last_carbon]);
+        last_carbon = next_carbon;
+
+        node_counter += 1;
+        if node_counter == num_nodes {
+            break;
+        }
+    }
+
+    (g.into_graph(), nodes, (num_nodes - 1).min(4))
+}
+
+
+fn distributed_randomized_coloring_algorithm(graph: &VecGraph, nodes: &mut Vec<Node>, delta: usize, verbose: bool) {
     // we have delta + 1 available color
     let list_of_colors: HashSet<Color> = (0..=delta).collect();
     assert_eq!(list_of_colors.len(), delta + 1);
 
     if verbose {
-        println!("Starting algorithm");
+        println!("Starting algorithm with delta = {delta}");
     }
     let mut round = 1;
     let mut rng = thread_rng();
@@ -115,7 +184,7 @@ fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<No
             nodes[v.index()].inbox.push((u.index(), c));
 
             if verbose {
-                println!("node {:3}: sending to node {:3}: ({}, {:?})", u.index(), v.index(), u.index(), c);
+                println!("node {:3}: sending to node {:3}:  {:?}", u.index(), v.index(), c);
             }
         }
 
@@ -140,6 +209,11 @@ fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<No
                 candidate_colors.remove(coloring.color());
             }
 
+            if verbose {
+                println!("node {:3}: non permanent colors {:?}", node.id, available_colors);
+                println!("node {:3}: colors not used by neighbors {:?}", node.id, candidate_colors);
+            }
+
             // reset inbox
             node.inbox.clear();
 
@@ -149,7 +223,7 @@ fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<No
                     println!("node {:3}: my color {:?} is used by nobody lets go permanent", node.id, node.coloring);
                 }
                 node.coloring = Permanent(*node.coloring.color());
-                break;
+                continue;
             }
 
             let random_color = available_colors.iter().choose(&mut rng).unwrap();
@@ -187,7 +261,7 @@ fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<No
 /// all colors has the same length as the vector containing all the nodes
 fn test_case(verbose: bool) {
     let (graph, mut nodes, delta) = complete_graph(200);
-    distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, verbose);
+    distributed_randomized_coloring_algorithm(&graph, &mut nodes, delta, verbose);
 
     println!("\n\nAlgorithm finished:");
     for node in nodes.iter_mut() {
@@ -211,16 +285,20 @@ fn test_case(verbose: bool) {
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Run mode
-    #[arg(value_enum, default_value_t = RunMode::Testcase)]
+    #[arg(short, long, value_enum, default_value_t = RunMode::Testcase)]
     mode: RunMode,
 
     /// Number of nodes to be used, has no effect for testcase run mode
-    #[arg(default_value_t = 1, value_parser = clap::value_parser ! (u64).range(1..))]
+    #[arg(short, long, default_value_t = 1, value_parser = clap::value_parser ! (u64).range(1..))]
     num: u64,
 
     /// Print additional information while running the algorithm
     #[arg(short, long)]
     verbose: bool,
+
+    /// Create a dot file of the graph to visualize with graphviz, has no effect for testcase run mode
+    #[arg(short, long)]
+    dotfile: Option<String>,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
@@ -228,6 +306,49 @@ enum RunMode {
     Testcase,
     CompleteGraph,
     Chain,
+    Hydrocarbon,
+}
+
+fn graph_to_dot(file_path: String, graph: VecGraph, nodes: &Vec<Node>, delta: usize, verbose: bool) {
+    if verbose {
+        println!("Writing dot file into '{}'", file_path);
+    }
+
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(file_path);
+
+    if file.is_err() {
+        panic!("Writing dot file failed: {:?}", file.err().unwrap());
+    }
+
+    let mut file = file.unwrap();
+    file.write("strict graph {\n".as_bytes()).unwrap();
+
+    for e in graph.edges() {
+        let (u, v) = graph.enodes(e);
+        file.write(format!("n{} -- n{}\n", u.index(), v.index()).as_bytes()).unwrap();
+    }
+
+    let unique_colors: Vec<String> = (0..=delta).into_iter().map(|_| {
+        let mut rng = thread_rng();
+        let dist = Uniform::new(0, 200);
+        format!("#{:02x}{:02x}{:02x}", rng.sample(dist), rng.sample(dist), rng.sample(dist))
+    }).collect();
+
+    if verbose {
+        println!("colors generated = {:#?}", unique_colors);
+    }
+
+    for (id, node) in nodes.iter().enumerate() {
+        let color = &unique_colors[*node.coloring.color()];
+        file.write(format!("n{} [color=\"black\", fillcolor=\"{}\", style=filled]\n", id, color).as_bytes()).unwrap();
+    }
+
+    file.write("}\n".as_bytes()).unwrap();
+    file.flush().unwrap();
 }
 
 fn main() {
@@ -241,18 +362,38 @@ fn main() {
         }
         RunMode::CompleteGraph => {
             let (graph, mut nodes, delta) = complete_graph(num_nodes);
-            distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, cli.verbose);
+            distributed_randomized_coloring_algorithm(&graph, &mut nodes, delta, cli.verbose);
 
             for node in nodes.iter_mut() {
                 println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
             }
+
+            if cli.dotfile.is_some() {
+                graph_to_dot(cli.dotfile.unwrap(), graph, &nodes, delta, cli.verbose);
+            }
         }
         RunMode::Chain => {
             let (graph, mut nodes, delta) = chain(num_nodes);
-            distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, cli.verbose);
+            distributed_randomized_coloring_algorithm(&graph, &mut nodes, delta, cli.verbose);
 
             for node in nodes.iter_mut() {
                 println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
+            }
+
+            if cli.dotfile.is_some() {
+                graph_to_dot(cli.dotfile.unwrap(), graph, &nodes, delta, cli.verbose);
+            }
+        }
+        RunMode::Hydrocarbon => {
+            let (graph, mut nodes, delta) = hydrocarbon(num_nodes);
+            distributed_randomized_coloring_algorithm(&graph, &mut nodes, delta, cli.verbose);
+
+            for node in nodes.iter_mut() {
+                println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
+            }
+
+            if cli.dotfile.is_some() {
+                graph_to_dot(cli.dotfile.unwrap(), graph, &nodes, delta, cli.verbose);
             }
         }
     }
