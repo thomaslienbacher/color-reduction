fcad284df3400415e0894b84a723bcf225a62333
Thomas Lienbacher <lienbacher.tom@gmail.com>
2024-01-14 18:00:28 +0100

    Implemented algorithm from assignment with cli

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # color-reduction
 
-This is my implementation for Assignment 2
-Exercise B
-from Design and Analysis of Algorithms, WS23.
-This includes an algorithm
-to half the coloring of a graph in O(delta) time.
+This is my implementation for a distributed randomized coloring algorithm
+for the assignment 3 bonus exercise from Design and Analysis of Algorithms, WS23.
 
+## How to build
+
+This project was created with [Rust](https://www.rust-lang.org/) version 1.75.0.
+
+1. Install Rust using [rustup](https://www.rust-lang.org/tools/install) (recommended)
+2. Clone this repository `git clone https://github.com/thomaslienbacher/color-reduction.git`
+3. Run `cargo run --release` in the repository folder, this will build and run the project
+
+The binary is placed in the `<repo>/target/release` folder and can alternatively 
+be just build using `cargo build --release`.
+
+## Previous version
+
+It also included my implementation for assignment 2
+exercise B in previous commits.
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
-use std::collections::{HashMap, HashSet};
-use rs_graph::{Builder, IndexGraph, VecGraph};
+use std::collections::HashSet;
+
+use rand::prelude::IteratorRandom;
+use rand::thread_rng;
+use rs_graph::{Builder, VecGraph};
 use rs_graph::traits::{FiniteGraph, Indexable};
 use rs_graph::vecgraph::VecGraphBuilder;
+use clap::{Parser, ValueEnum};
+
+use crate::Coloring::{Candidate, Permanent};
+
+type Color = usize;
+
+#[derive(Copy, Clone, Debug)]
+enum Coloring {
+    Permanent(Color),
+    Candidate(Color),
+}
+
+impl Coloring {
+    fn color(&self) -> &Color {
+        match self {
+            Permanent(v) => { v }
+            Candidate(v) => { v }
+        }
+    }
+}
 
 #[derive(Clone, Debug)]
 struct Node {
     id: usize,
-    color: usize,
-    inbox: Vec<(usize, usize)>,
+    coloring: Coloring,
+    inbox: Vec<(usize, Coloring)>,
 }
 
 fn N(id: usize) -> Node {
     Node {
         id,
-        color: id,
+        coloring: Candidate(id),
         inbox: Vec::new(),
     }
 }
 
+/// creates a complete graph with `num_nodes` vertices
+/// the graph has max degree `num_nodes`
+/// returns the graph, a vector of nodes and delta (max degree)
 fn complete_graph(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
     let mut nodes = Vec::with_capacity(num_nodes);
     let mut g = VecGraphBuilder::new();
@@ -36,6 +62,9 @@ fn complete_graph(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
     (g.into_graph(), nodes, delta)
 }
 
+/// creates a graph that is a chain of vertices with `num_nodes` vertices
+/// the degree has max degree 2
+/// returns the graph, a vector of nodes and delta (max degree)
 fn chain(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
     let mut nodes = Vec::with_capacity(num_nodes);
     let mut g = VecGraphBuilder::new();
@@ -54,135 +83,177 @@ fn chain(num_nodes: usize) -> (VecGraph, Vec<Node>, usize) {
     (g.into_graph(), nodes, 2)
 }
 
-/// returns max color used
-fn apply_halving(graph: &VecGraph, nodes: &mut Vec<Node>, delta: usize) -> usize {
-    println!("Start algorithm: ");
-    let mut r = 1;
+fn distributed_randomized_coloring_algorithm(graph: VecGraph, nodes: &mut Vec<Node>, delta: usize, verbose: bool) {
+    // we have delta + 1 available color
+    let list_of_colors: HashSet<Color> = (0..=delta).collect();
+    assert_eq!(list_of_colors.len(), delta + 1);
 
-    // first round calculate own color / 2 and send to all others
+    if verbose {
+        println!("Starting algorithm");
+    }
+    let mut round = 1;
+    let mut rng = thread_rng();
+
+    // in the first round no node has a permanent color, so everybody chooses a random color
     for node in nodes.iter_mut() {
-        node.color = node.color.div_ceil(2);
-        //println!("node {} has new color {}", node.id, node.color);
+        let random_color = list_of_colors.iter().choose(&mut rng).unwrap();
+        node.coloring = Candidate(*random_color);
+        if verbose {
+            println!("node {:3} chose color {:?}", node.id, node.coloring);
+        }
     }
 
-    for e in graph.edges() {
-        let (u, v) = graph.enodes(e);
-        let c = nodes[u.index()].color;
-        nodes[v.index()].inbox.push((u.index(), c));
-        //println!("node {}: sending to node {}: ({}, {})", u.index(), v.index(), u.index(), c);
-    }
-    r += 1;
-
-    // loop until all discrepancies are fixed
-    for _ in 0..delta {
-        println!("\nstarting round {r}");
-
-        // process messages and calculate new color if needed
-        for node in nodes.iter_mut() {
-            //println!("node {}: doing processing", node.id);
-            let mut neighbor_color_set = HashSet::new();
-            for (_, c) in &node.inbox {
-                neighbor_color_set.insert(c);
-            }
-
-            // check is my color in the neighbor set
-            if !neighbor_color_set.contains(&node.color) {
-                //println!("node {}: i have a good color :)", node.id);
-                node.inbox.clear();
-                continue;
-            } else {
-                println!("node {:3}: somebody is also using my color", node.id);
-            }
-
-            // get max id of problematic color
-            let mut max_id = 0;
-            let mut all_colors = node.inbox.clone();
-            all_colors.push((node.id, node.color));
-            for (id, c) in all_colors {
-                if c == node.color {
-                    max_id = max_id.max(id);
-                }
-            }
-            println!("max id node with color {} is node {}", node.color, max_id);
-
-            // check if i am someone that needs to change the color
-            if max_id == node.id {
-                //println!("node {}: i am the problematic node :(", node.id);
-
-                // find smallest number that is not in neighborcolor set
-                let mut not_used = 0;
-                while neighbor_color_set.contains(&not_used) {
-                    not_used += 1;
-                }
-
-                node.color = not_used;
-                println!("node {:3}: setting my new color to {:3}", node.id, node.color);
-            } else {
-                //println!("node {}: i am NOT the problematic node", node.id);
-            }
-
-            node.inbox.clear();
+    loop {
+        if verbose {
+            println!("\nStarting round {round}");
         }
 
-        // send colors to all neighbors
+        // exchange color with all neighbors
         for e in graph.edges() {
             let (u, v) = graph.enodes(e);
-            let c = nodes[u.index()].color;
+            let c = nodes[u.index()].coloring;
             nodes[v.index()].inbox.push((u.index(), c));
-            //println!("node {}: sending to node {}: ({}, {})", u.index(), v.index(), u.index(), c);
+
+            if verbose {
+                println!("node {:3}: sending to node {:3}: ({}, {:?})", u.index(), v.index(), u.index(), c);
+            }
         }
 
-        println!("\nround colors:");
+        let has_candidate_color = |n: &&mut Node| match n.coloring {
+            Candidate(_) => true,
+            Permanent(_) => false
+        };
+
+        // for all non permanent nodes compute available set of colors and permanently color if possible
+        // if not do next iteration and choose new random color
+        for node in nodes.iter_mut().filter(has_candidate_color) {
+            if verbose {
+                println!("node {:3} is none permanent", node.id);
+            }
+            let mut available_colors = list_of_colors.clone();
+            let mut candidate_colors = list_of_colors.clone();
+
+            for (_, coloring) in &node.inbox {
+                if let Permanent(v) = coloring {
+                    available_colors.remove(v);
+                }
+                candidate_colors.remove(coloring.color());
+            }
+
+            // reset inbox
+            node.inbox.clear();
+
+            // check if node can go permanent
+            if candidate_colors.contains(node.coloring.color()) {
+                if verbose {
+                    println!("node {:3}: my color {:?} is used by nobody lets go permanent", node.id, node.coloring);
+                }
+                node.coloring = Permanent(*node.coloring.color());
+                break;
+            }
+
+            let random_color = available_colors.iter().choose(&mut rng).unwrap();
+            node.coloring = Candidate(*random_color);
+
+            if verbose {
+                println!("node {:3} cannot be fixed chose new color {:?}", node.id, node.coloring);
+            }
+        }
+
+        // check if the graph has a valid coloring
+        if nodes.iter_mut().filter(has_candidate_color).next().is_none() {
+            if verbose {
+                println!("no candidate colors left, coloring should be fixed");
+                println!("Finished after {round} rounds\n");
+            }
+            break;
+        }
+
+        // print new coloring
         for node in nodes.iter_mut() {
-            println!("node {:3} has color {:3}", node.id, node.color);
+            if verbose {
+                println!("node {:3} has color {:?}", node.id, node.coloring);
+            }
         }
 
-        r += 1;
+        round += 1;
     }
+}
 
-    //println!("\nfinal colors:");
+
+/// this is the test case, it generates a complete graph with 200 vertices
+/// in such a case each color may only be used once
+/// we check this by checking the length of the deduplicated vector containing
+/// all colors has the same length as the vector containing all the nodes
+fn test_case(verbose: bool) {
+    let (graph, mut nodes, delta) = complete_graph(200);
+    distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, verbose);
+
+    println!("\n\nAlgorithm finished:");
     for node in nodes.iter_mut() {
-        //println!("node {:3} has color {:3}", node.id, node.color);
+        println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
     }
 
-    println!("\nfinal colors sorted:");
-    //nodes.sort_by(|n, b| n.color.cmp(&b.color));
+    // in a complete graph, each color must only be used once
+    nodes.sort_by(|a, b| a.coloring.color().cmp(&b.coloring.color()));
+    println!("\nSorting by color:");
     for node in nodes.iter_mut() {
-        println!("node {:3} has color {:3}", node.id, node.color);
+        println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
     }
 
-    nodes.iter().max_by(|a, b| a.color.cmp(&b.color)).unwrap().color
+    // the length must be the same after the deduplication
+    let all_nodes_len = nodes.len();
+    nodes.dedup_by_key(|n| *n.coloring.color());
+    assert_eq!(nodes.len(), all_nodes_len);
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Run mode
+    #[arg(value_enum, default_value_t = RunMode::Testcase)]
+    mode: RunMode,
+
+    /// Number of nodes to be used, has no effect for testcase run mode
+    #[arg(default_value_t = 1, value_parser = clap::value_parser ! (u64).range(1..))]
+    num: u64,
+
+    /// Print additional information while running the algorithm
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum RunMode {
+    Testcase,
+    CompleteGraph,
+    Chain,
 }
 
 fn main() {
-    let (graph, mut nodes, delta) = chain(4);
+    let cli = Cli::parse();
+    let num_nodes = cli.num as usize;
+    println!("Running in {:?} mode with {num_nodes} vertices", cli.mode);
 
-    /*println!("delta = {delta}");
-    println!("Graph edges: ");
-    for e in graph.edges() {
-        let (u, v) = graph.enodes(e);
-        println!("({}, {})", u.index(), v.index());
-    }*/
-
-    let mut halvings = 0;
-    loop {
-        let new_max = apply_halving(&graph, &mut nodes, delta);
-        halvings += 1;
-        println!("new coloring : = {new_max} at {halvings}");
-        if new_max <= delta + 1 {
-            break;
+    match cli.mode {
+        RunMode::Testcase => {
+            test_case(cli.verbose);
         }
-        break;
-    }
+        RunMode::CompleteGraph => {
+            let (graph, mut nodes, delta) = complete_graph(num_nodes);
+            distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, cli.verbose);
 
-    //println!("other: {}", delta * delta + log_star(1000 * 29770000))
-}
+            for node in nodes.iter_mut() {
+                println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
+            }
+        }
+        RunMode::Chain => {
+            let (graph, mut nodes, delta) = chain(num_nodes);
+            distributed_randomized_coloring_algorithm(graph, &mut nodes, delta, cli.verbose);
 
-fn log_star(mut n: usize) -> usize {
-    let mut i = 0;
-    while n > 2 {
-        n = n.ilog2() as usize;
-        i += 1;
+            for node in nodes.iter_mut() {
+                println!("node {:3} has permanent color {:3}", node.id, node.coloring.color());
+            }
+        }
     }
-    i
 }
